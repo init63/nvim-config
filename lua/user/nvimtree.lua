727b81650bc9b2140a4661f7731b88aaf5b08f00
init63 <shandy4wd@mail.ru>
2022-04-18 10:55:07 +0400

    edit settings nvim-tree

--- lua/user/nvimtree.lua
+++ lua/user/nvimtree.lua
@@ -1,107 +1,83 @@
-require('nvim-tree').setup {
-  auto_reload_on_write = true,
-  disable_netrw = false,
-  hide_root_folder = false,
-  hijack_cursor = false,
-  hijack_netrw = true,
-  hijack_unnamed_buffer_when_opening = false,
-  ignore_buffer_on_setup = false,
-  open_on_setup = false,
-  open_on_setup_file = false,
+vim.g.nvim_tree_icons = {
+  default = "",
+  symlink = "",
+  git = {
+    unstaged = "",
+    staged = "S",
+    unmerged = "",
+    renamed = "➜",
+    deleted = "",
+    untracked = "U",
+    ignored = "◌",
+  },
+  folder = {
+    default = "",
+    open = "",
+    empty = "",
+    empty_open = "",
+    symlink = "",
+  },
+}
+
+local nvim_tree = require('nvim-tree')
+local nvim_tree_config = require('nvim-tree.config')
+local tree_cb = nvim_tree_config.nvim_tree_callback
+
+nvim_tree.setup {
+  update_to_buf_dir = {
+    enable = false,
+  },
+  ignore_ft_on_setup = {
+    "startify",
+    "dashboard",
+    "alpha",
+  },
+  open_on_setup_file = true,
   open_on_tab = true,
-  sort_by = "name",
-  update_cwd = false,
-  view = {
-    width = 30,
-    height = 30,
-    side = "left",
-    preserve_window_proportions = false,
-    number = false,
-    relativenumber = false,
-    signcolumn = "yes",
-    mappings = {
-      custom_only = false,
-      list = {
-        -- user mappings go here
-      },
-    },
-  },
-  renderer = {
-    indent_markers = {
-      enable = false,
-      icons = {
-        corner = "└ ",
-        edge = "│ ",
-        none = "  ",
-      },
-    },
-  },
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
-  update_focused_file = {
-    enable = false,
-    update_cwd = false,
-    ignore_list = {},
-  },
-  ignore_ft_on_setup = {},
-  system_open = {
-    cmd = nil,
-    args = {},
-  },
+  update_cwd = true,
   diagnostics = {
-    enable = false,
-    show_on_dirs = false,
+    enable = true,
     icons = {
-      hint = "",
+      hint = "",
       info = "",
       warning = "",
       error = "",
     },
   },
-  filters = {
-    dotfiles = false,
-    custom = {},
-    exclude = {},
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+    ignore_list = {},
   },
   git = {
     enable = true,
     ignore = true,
-    timeout = 400,
+    timeout = 500,
   },
-  actions = {
-    use_system_clipboard = true,
-    change_dir = {
-      enable = true,
-      global = false,
-    },
-    open_file = {
-      quit_on_open = false,
-      resize_window = false,
-      window_picker = {
-        enable = true,
-        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-        exclude = {
-          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
-          buftype = { "nofile", "terminal", "help" },
-        },
+  view = {
+    width = 30,
+    height = 30,
+    hide_root_folder = false,
+    side = "left",
+    auto_resize = true,
+    mappings = {
+      custom_only = false,
+      list = {
+        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+        { key = "h", cb = tree_cb "close_node" },
+        { key = "v", cb = tree_cb "vsplit" },
       },
     },
+    number = false,
+    relativenumber = false,
   },
-  trash = {
-    cmd = "trash",
-    require_confirm = true,
-  },
-  log = {
-    enable = false,
-    truncate = false,
-    types = {
-      all = false,
-      config = false,
-      copy_paste = false,
-      git = false,
-      profile = false,
-    },
+  quit_on_open = 0,
+  git_hl = 1,
+  show_icons = {
+    git = 1,
+    folders = 1,
+    files = 1,
+    folder_arrows = 1,
+    tree_width = 30,
   },
 }
