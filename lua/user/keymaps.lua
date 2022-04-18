81117d9bba7d22b1ece178527724f36fa0934bc4
init63 <shandy4wd@mail.ru>
2022-04-18 09:41:53 +0400

    add prettier keymap

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -2,3 +2,7 @@ local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 keymap('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
+
+-- prettier
+keymap('v', '<leader>f', '<Plug>(coc-format-selected)', opts)
+keymap('n', '<leader>f', '<Plug>(coc-format-selected)', opts)
